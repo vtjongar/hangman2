d3fa077cbc9d74ad2dcd00a77223ce0201ca4742
Vanessa Henderson <bookvanny@gmail.com>
2019-01-08 12:32:57 -0500

    can target input indicies

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ let input = document.querySelector('.input')
 const button = document.querySelector('.button')
 const sample = document.querySelector(".sample");
 
-button.addEventListener('click', function(evt) {
+// button.addEventListener('click', function(evt) {
 
-    // var inputValue = input.value
-    console.log("hello!")
+       // to get input to appear on screen 
+
+//     evt.preventDefault();
+    // var paragraph = document.createElement("p")
+    // var text = document.createTextNode(input.value);
+    // paragraph.appendChild(text);
+    // sample.appendChild(paragraph);
+
+// })
+
+
+button.addEventListener('click', function(evt) {
     evt.preventDefault();
+    var word = input.value
+    var one = word.charAt(0);
+    
     var paragraph = document.createElement("p")
-    var text = document.createTextNode(input.value);
+    var text = document.createTextNode(one);
     paragraph.appendChild(text);
     sample.appendChild(paragraph);
 
-
 })
+
+   
